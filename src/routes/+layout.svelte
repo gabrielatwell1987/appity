d43060c587e9e70789c01cc719a986aa7b3a8102
Gabriel Atwell <gabrielatwell@proton.me>
2024-02-29 13:24:47 -0800

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,7 +20,7 @@
 <AppShell>
 	<svelte:fragment slot="header">
 		<AppBar>
-			<h1 class="text-left margin">Snippet Creator</h1>
+			<h1 class="margin">Snippet Creator</h1>
 		</AppBar>
 	</svelte:fragment>
 
