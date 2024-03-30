0a0463880012b543fdda7b3901902faa89a85064
Gabriel Atwell <gabrielatwell@proton.me>
2024-03-30 16:04:22 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,14 +15,14 @@
 
 <div class="flex justify-center">
 	<div class="grid grid-cols-1 gap-4 min-w-full md:min-w-[750px]">
-		<h3 class="text-center py-6 text-3xl">Create a Snippet</h3>
+		<h3 class="text-center py-6 text-3xl font-bold">Create a Snippet</h3>
 		<div class="card p-4 w-full text-token space-y-4 rounded">
 			<label class="label">
 				<span>Snippet Title</span>
 				<input type="text" class="input rounded" placeholder="Enter title here..." bind:value={formData.title}>
 			</label>
 			<label class="label">
-				<span>Programming Language</span>
+				<span class="font-semibold">Programming Language</span>
 				<select class="select rounded"  bind:value={formData.language}>
 					<option value="html">HTML</option>
 					<option value="css">CSS</option>
@@ -30,13 +30,13 @@
 				</select>
 			</label>
 			<label class="label">
-				<span>Main Snippet</span>
+				<span class="font-semibold">Main Snippet</span>
 				<textarea rows="4" class="textarea rounded" placeholder="Enter your code snippet here..." bind:value={formData.code}></textarea>
 			</label>
-			<button type="button" class="btn btn-sm variant-filled-primary rounded" on:click={() => addSnippet(formData)}>Create Snippet</button>
+			<button type="button" class="btn btn-sm variant-filled-primary rounded font-bold" on:click={() => addSnippet(formData)}>Create Snippet</button>
 		</div>
 		<div class="text-center py-6">
-			<h2 class="text-3xl">Code Snippets</h2>
+			<h2 class="text-3xl font-semibold">Code Snippets</h2>
 		</div>
 		{#each $snippetStore as snippet, index}
 			<CodeSnippetCard {snippet} {index} />
